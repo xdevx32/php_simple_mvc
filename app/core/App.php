b2b3878f9d81ad122e6e694392c4daef15b39480
xdevx32 <angelkukushev333@gmail.com>
2018-10-09 16:53:55 +0300

    Controllers, HTACCESS

--- app/core/App.php
+++ app/core/App.php
@@ -2,8 +2,22 @@
 
 class App
 {
+
+	protected $controller = 'home';
+
+	protected $method = 'index';
+
+	protected $params = [];
+
 	public function __construct()
 	{
-		echo 'OK!';
+		$url = $this->parseUrl();
+	}
+
+	public function parseUrl()
+	{
+		if(isset($_GET['url'])) {
+			return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+		}
 	}
 }
