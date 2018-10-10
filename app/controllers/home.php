6e4185da8daa02fe0fb959c48c4f02fef3151623
xdevx32 <angelkukushev333@gmail.com>
2018-10-10 17:40:48 +0300

    Finished setting up MVC

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -5,7 +5,9 @@ class Home extends Controller
 	
 	public function index($name = '')
 	{
-		echo $name;
+		$user = $this->model('User');
+		$user->name = $name;
+		$this->view('home/index', ['name' => $user->name]);
 	}
 	
 }
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,7 +4,7 @@ class Controller
 {
 	public function model($model)
 	{
-		require_once '../app/models' . $model . '.php';
+		require_once '../app/models/' . $model . '.php';
 		return new $model();
 	}
 
